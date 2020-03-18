e9236281b2ea7ab9a3a4a5c579190c3aa56275c6
Edward <ed19911127@gmail.com>
2020-03-17 22:22:08 -0700

    Updates

---
+++ precache-manifest.13ea9d9196eb2e16954c56b58f59ca9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "995b5dfa17cc0d127675a963ce993d79",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "3dc8e89824fa47ea1a4d",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "bb8752300280c3de1eb2",
+    "url": "/robofriends/static/css/main.162db726.chunk.css"
+  },
+  {
+    "revision": "3dc8e89824fa47ea1a4d",
+    "url": "/robofriends/static/js/2.58272216.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/robofriends/static/js/2.58272216.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb8752300280c3de1eb2",
+    "url": "/robofriends/static/js/main.e6010fb8.chunk.js"
+  },
+  {
+    "revision": "6a5eb8d065c05feaff0d",
+    "url": "/robofriends/static/js/runtime-main.5530ba65.js"
+  }
+]);
